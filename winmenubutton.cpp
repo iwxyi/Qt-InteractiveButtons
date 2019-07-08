2f763fa97420b29d4c3a7a6d8e9035df3de6400c
MRXY001 <wxy19980615@gmail.com>
2019-07-08 15:41:03 +0800

    添加菜单按钮响应鼠标移动事件

--- winmenubutton.cpp
+++ winmenubutton.cpp
@@ -11,6 +11,7 @@ void WinMenuButton::paintEvent(QPaintEvent *event)
     InteractiveButtonBase::paintEvent(event);
 
     int w = geometry().width(), h = geometry().height();
+    int dx = offset_pos.x(), dy = offset_pos.y();
 
     QPainter painter(this);
     painter.setPen(QPen(icon_color));
@@ -23,13 +24,13 @@ void WinMenuButton::paintEvent(QPaintEvent *event)
 
         int len = w/3;
         int l = w/3, r = w*2/3, t = h/3 + pro*h/3;
-        painter.drawLine(QPoint(l,t), QPoint(r,t));
+        painter.drawLine(QPoint(l+dx/4,t+dy/4), QPoint(r+dx/4,t+dy/4));
 
         l = w/3+pro*w/24, r = w*2/3-pro*w/24, t = w/2+pro*h*5/18;
-        painter.drawLine(QPoint(l,t), QPoint(r,t));
+        painter.drawLine(QPoint(l+dx/2,t+dy/2), QPoint(r+dx/2,t+dy/2));
 
         l = w/3+pro*w/12, r = w*2/3-pro*w/12, t=w*2/3+pro*h*2/9;
-        painter.drawLine(QPoint(l,t), QPoint(r,t));
+        painter.drawLine(QPoint(l+dx,t+dy), QPoint(r+dx,t+dy));
 
         /*int half_len = w/6;//quick_sqrt(w/3*w/3 + h/3*h/3) / 2; // 长度
         painter.setRenderHint(QPainter::Antialiasing,true);
@@ -75,18 +76,18 @@ void WinMenuButton::paintEvent(QPaintEvent *event)
     }
     else if (getState())
     {
-        painter.drawLine(w/3,h*2/3, w*2/3,h*2/3);
-        painter.drawLine(w/3+w/24, h*7/9, w*2/3-w/24, h*7/9);
-        painter.drawLine(w/3+w/12, h*8/9, w*2/3-w/12, h*8/9);
+        painter.drawLine(w/3+dx/4,h*2/3+dy/4, w*2/3+dx/4,h*2/3+dy/4);
+        painter.drawLine(w/3+w/24+dx/2, h*7/9+dy/2, w*2/3-w/24+dx/2, h*7/9+dy/2);
+        painter.drawLine(w/3+w/12+dx, h*8/9+dy, w*2/3-w/12+dx, h*8/9+dy);
 
         /*painter.drawLine(QPoint(0.39*w, 0.39*h), QPoint(0.61*w, 0.61*h));
         painter.drawLine(QPoint(0.39*w, 0.61*h), QPoint(0.61*w, 0.39*h));*/
     }
     else
     {
-        painter.drawLine(QPoint(w/3,h/3), QPoint(w*2/3,h/3));
-        painter.drawLine(QPoint(w/3,h/2), QPoint(w*2/3,h/2));
-        painter.drawLine(QPoint(w/3,h*2/3), QPoint(w*2/3,h*2/3));
+        painter.drawLine(QPoint(w/3+dx/4,h/3+dy/4), QPoint(w*2/3+dx/4,h/3+dy/4));
+        painter.drawLine(QPoint(w/3+dx/2,h/2+dy/2), QPoint(w*2/3+dx/2,h/2+dy/2));
+        painter.drawLine(QPoint(w/3+dx,h*2/3+dy), QPoint(w*2/3+dx,h*2/3+dy));
     }
 }
 
